b2fe708c28c8894f68588328bc51ef2b065a87bd
Leonel Dominguez <leonelchavez2013@hotmail.com>
2021-02-25 23:20:09 -0600

    Arreglo de la clase conexión

--- SistemaHotelero/src/main/java/seguridad/datos/Conexion.java
+++ SistemaHotelero/src/main/java/seguridad/datos/Conexion.java
@@ -8,7 +8,7 @@ public class Conexion {
 
     private static final String JDBC_URL = "jdbc:mysql://localhost/umg?useSSL=false&serverTimezone=UTC";
     private static final String JDBC_USER = "root";
-vate static final String JDBC_PASS = "";
+    private static final String JDBC_PASS = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
