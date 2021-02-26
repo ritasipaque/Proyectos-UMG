d639cdf4f4b69eebd19ae20cac90283d89d15f10
Diana-Victores <dcpernillo@gmail.com>
2021-02-26 01:09:23 -0600

    LOGIN

---
+++ SistemaHotelero/src/main/java/modelo/Usuarios.java
@@ -0,0 +1,99 @@
+
+package modelo;
+
+public class Usuarios {
+    
+    private int id;
+    private String usuario;
+    private String password;
+    private String nombre;
+    private String email;
+    private String last_session;
+    private String txtRespuesta;
+    private int idTipo;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+   
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    public void setRespuesta(String Respuesta) {
+        this.txtRespuesta=Respuesta;
+    }
+    
+     public void getRespuesta (String Respuesta) {
+        this.txtRespuesta=Respuesta;
+    }
+
+//******************************************************************************     
+     
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLast_session() {
+        return last_session;
+    }
+
+    public void setLast_session(String last_session) {
+        this.last_session = last_session;
+    }
+ //**********************************************************************************
+    public String getRespuesta() {
+        return txtRespuesta;
+    }
+
+     public void setRespuesta(int Respuesta) {
+        this.txtRespuesta = txtRespuesta;
+    }
+//*****************************************************************************************
+    public int getIdTipo() {
+        return idTipo;
+    }
+
+    public void setIdTipo(int idTipo) {
+        this.idTipo = idTipo;
+    }
+    
+
+
+    
+
+   
+
+}
