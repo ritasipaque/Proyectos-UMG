9614922684d0ea071d86a9586e755b5732f81ab1
DiegoVasquez2001IS <gamer070lol2@gmail.com>
2021-04-15 08:38:55 -0600

    Terminando cosas, hay un error en buscar

--- SistemaHotelero/src/main/java/Finanzas/datos/ClasificacionCuentaDAO.java
+++ SistemaHotelero/src/main/java/Finanzas/datos/ClasificacionCuentaDAO.java
@@ -4,6 +4,7 @@ import Finanzas.dominio.ClasificacionCuenta;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
 import javax.swing.JOptionPane;
 
 /**
@@ -13,9 +14,9 @@ import javax.swing.JOptionPane;
 public class ClasificacionCuentaDAO extends Conexion {
 
     private String SQL_INSERT = "INSERT INTO ClasificacionCuenta (Codigo_clasificacion, Clasificacion_CuentaNombre, Descripcion_Clasificacion) VALUES(?,?,?)";
-    private String SQL_UPDATE = "UPDATE ClasificacionCuenta SET CodigoClasificacion=?, Clasificacion_CuentaNombre = ?, Descripcion_Clasificacion = ?";
-    private String SQL_QUERY = "SELECT * FROM ClasificacionCuenta WHERE CodigoClasificacion = ?";
-    private String SQL_DELETE = "DELETE FROM ClasificacionCuenta WHERE CodigoClasificacion = ?";
+    private String SQL_UPDATE = "UPDATE ClasificacionCuenta SET Codigo_clasificacion=?, Clasificacion_CuentaNombre = ?, Descripcion_Clasificacion = ?";
+    private String SQL_QUERY = "SELECT * FROM ClasificacionCuenta WHERE Codigo_clasificacion = ?";
+    private String SQL_DELETE = "DELETE FROM ClasificacionCuenta WHERE Codigo_clasificacion = ?";
     private String SQL_SELECT = "SELECT * FROM ClasificacionCuenta";
 
     public int Insertar(ClasificacionCuenta objClasificacion) {
@@ -36,20 +37,114 @@ public class ClasificacionCuentaDAO extends Conexion {
 
             if (row >= 1) {
                 flagRegistro = 1;
-                JOptionPane.showMessageDialog(null, "¡REGISTRO EXITOSO!", "INFORMACIÓN", JOptionPane.INFORMATION_MESSAGE);
             } else {
                 flagRegistro = 0;
-                JOptionPane.showMessageDialog(null, "¡REGISTRO ERRÓNEO!", "ERROR", JOptionPane.ERROR_MESSAGE);
             }
 
         } catch (Exception ex) {
             System.out.println(ex);
-            JOptionPane.showMessageDialog(null, "¡REGISTRO ERRÓNEO!", "ERROR", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "¡ERROR INTERNO, CONSULTE CON EL ADMINISTRADOR!", "ERROR", JOptionPane.ERROR_MESSAGE);
         } finally {
             Conexion.close(con);
             Conexion.close(stmt);
         }
         return flagRegistro;
+    }
+
+    public int Actualizar(ClasificacionCuenta objClasificacion) {
+        int flagRegistro = 0;
+
+        Connection con = null;
+        PreparedStatement stmt = null;
+        int row = 0;
+
+        try {
+            con = Conexion.getConnection();
+            stmt = con.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, objClasificacion.getCodigoClasificacion());
+            stmt.setString(2, objClasificacion.getClasificacionCuenta());
+            stmt.setString(3, objClasificacion.getDescripcionClasificacion());
+            row = stmt.executeUpdate();
+
+            if (row >= 1) {
+                flagRegistro = 1;
+            } else {
+                flagRegistro = 0;
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+            JOptionPane.showMessageDialog(null, "¡ERROR INTERNO, CONSULTE CON EL ADMINISTRADOR!", "ERROR", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            Conexion.close(con);
+            Conexion.close(stmt);
+        }
+        return flagRegistro;
+    }
+
+    public int Eliminar(ClasificacionCuenta objClasificacion) {
+        int flagRegistro = 0;
+
+        Connection con = null;
+        PreparedStatement stmt = null;
+
+        int row = 0;
+
+        try {
+            con = Conexion.getConnection();
+            stmt = con.prepareStatement(SQL_DELETE);
+            stmt.setString(1, objClasificacion.getCodigoClasificacion());
+            row = stmt.executeUpdate();
+
+            if (row >= 1) {
+                flagRegistro = 1;
+            } else {
+                flagRegistro = 0;
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+            JOptionPane.showMessageDialog(null, "¡ERROR INTERNO, CONSULTE CON EL ADMINISTRADOR!", "ERROR", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            Conexion.close(con);
+            Conexion.close(stmt);
+        }
+        return flagRegistro;
+    }
+
+    public ClasificacionCuenta Buscar(ClasificacionCuenta objClasificacion) {
+
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            con = Conexion.getConnection();
+            stmt = con.prepareStatement(SQL_QUERY);
+            stmt.setString(1, objClasificacion.getCodigoClasificacion());
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                String codigoClasificacion = rs.getString("Codigo_clasificacion");
+                String clasificacionCuenta = rs.getString("Clasificacion_CuentaNombre");
+                String descripcionClasificacion = rs.getString("Descripcion_Clasificacion");
+                
+                objClasificacion = new ClasificacionCuenta();
+                
+                objClasificacion.setCodigoClasificacion(codigoClasificacion);
+                objClasificacion.setClasificacionCuenta(clasificacionCuenta);
+                objClasificacion.setDescripcionClasificacion(descripcionClasificacion);
+                
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+            JOptionPane.showMessageDialog(null, "¡ERROR INTERNO, CONSULTE CON EL ADMINISTRADOR!", "ERROR", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            Conexion.close(con);
+            Conexion.close(stmt);
+        }
+
+        return objClasificacion;
     }
 
     /*public int getCantidadRegistros() {
@@ -80,7 +175,7 @@ public class ClasificacionCuentaDAO extends Conexion {
         return cantidadRegistros;
     }*/
 
-    /*public String[][] TablaDespliegue() {
+ /*public String[][] TablaDespliegue() {
         String[][] matrixClasificacion;
         matrixClasificacion = new String[getCantidadRegistros()][3];
 
