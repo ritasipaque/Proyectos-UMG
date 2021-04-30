03eb8bd4016851e041691e6dbace34eb939578e5
DiegoVasquez2001IS <gamer070lol2@gmail.com>
2021-04-29 23:54:39 -0600

    Finanzas

--- SistemaHotelero/src/main/java/Finanzas/vista/TransaccionEncabAsientoCont.java
+++ SistemaHotelero/src/main/java/Finanzas/vista/TransaccionEncabAsientoCont.java
@@ -31,11 +31,11 @@ public class TransaccionEncabAsientoCont extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGap(0, 652, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGap(0, 280, Short.MAX_VALUE)
         );
 
         pack();
